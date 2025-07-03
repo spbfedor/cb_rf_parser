0bd35fd4cb2d89d6863fe03eaba2860d0f0513cc
spbfedor <spbfedor@yandex.ru>
2025-07-03 22:33:33 +0300

    Спарсил с сайта xml и создал словарь с курсом валют

--- main.py
+++ main.py
@@ -9,4 +9,10 @@ current_date = datetime.date.today().strftime('%d.%m.%Y')
 url = f'https://www.cbr.ru/scripts/XML_daily.asp?date_req={current_date}'
 
 response = requests.get(url)
-print(response.text)
+
+root = ET.fromstring(response.content)
+valute_dict = {}
+for valute in root.iter('Valute'):
+    valute_dict[valute[3].text] = valute[4].text
+
+print(valute_dict['Евро'])
