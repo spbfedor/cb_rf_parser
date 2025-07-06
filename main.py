9938e6bc29488dd7459855713ce7895306a252ca
spbfedor <spbfedor@yandex.ru>
2025-07-06 19:19:38 +0300

    Черновой готовый вариант

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ import datetime
 import requests
 import xml.etree.ElementTree as ET
 
-ID_EURO = 'R01239'
-ID_US_DOLLAR = 'R01235'
 
 current_date = datetime.date.today().strftime('%d.%m.%Y')
 url = f'https://www.cbr.ru/scripts/XML_daily.asp?date_req={current_date}'
@@ -15,4 +13,5 @@ valute_dict = {}
 for valute in root.iter('Valute'):
     valute_dict[valute[3].text] = valute[4].text
 
-print(valute_dict['Евро'])
+print('Евро', valute_dict['Евро'])
+print('Доллар США', valute_dict['Доллар США'])
